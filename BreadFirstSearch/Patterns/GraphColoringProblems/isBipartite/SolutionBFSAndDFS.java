05bf673c65b606710b7e4c9e437ba5d6f28d411a
arun <arunsadhasivam@yahoo.co.in>
2019-11-27 17:11:25 -0800

    Update SolutionBFSAndDFS.java

--- BreadFirstSearch/Patterns/GraphColoringProblems/isBipartite/SolutionBFSAndDFS.java
+++ BreadFirstSearch/Patterns/GraphColoringProblems/isBipartite/SolutionBFSAndDFS.java
@@ -5,19 +5,19 @@ class Solution {
          
         for (int i = 0; i < g.length; i++){
             if (colors[i] == 0) {
-              if(!bfs(g,colors,i)) return false;  
-              //if(!dfs(g,i,colors,1)) return false;
+              //if(!bfs(g,colors,i,1)) return false;  
+              if(!dfs(g,colors,i,1)) return false;
             }
         }
         return true;
     }
     
     //1 ms, faster than 65.71% 
-    //44.7 MB, less than 63.42%
-    private boolean bfs(int[][] g,int[] colors ,int i ){
+    //44.9 MB, less than 60.98%
+    private boolean bfs(int[][] g,int[] colors ,int index,int color ){
         Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        colors[i] = 1;
+        q.add(index);
+        colors[index] = 1;
         while (!q.isEmpty()) {
             Integer node = q.poll();
             for (int adjacent : g[node]){
@@ -36,15 +36,15 @@ class Solution {
     //Runtime: 0 ms, faster than 100.00% of Java online submissions .
     //Memory Usage: 44.2 MB, less than 63.42% of Java online submissions .
 
-    private boolean dfs(int[][] graph, int current, int[] colors, int color) {
-        if (colors[current] == -color)
+    private boolean dfs(int[][] graph, int[] colors, int index,int color) {
+        if (colors[index] == -color)
             return false;
-        colors[current] = color;
-        for (int other: graph[current]) {
+        colors[index] = color;
+        for (int other: graph[index]) {
             if (colors[other] == color)
                 return false;
             if (colors[other] == 0)
-                if (!dfs(graph, other, colors, -color))
+                if (!dfs(graph,colors, other, -color))
                     return false;
         }
         return true;
