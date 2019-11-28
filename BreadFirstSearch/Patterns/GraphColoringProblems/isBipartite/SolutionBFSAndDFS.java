c1aa45be8b88837823d03c07282d0784d863c48a
arun <arunsadhasivam@yahoo.co.in>
2019-11-27 17:07:22 -0800

    Update and rename SolutionBFS.java to SolutionBFSAndDFS.java

--- BreadFirstSearch/Patterns/GraphColoringProblems/isBipartite/SolutionBFSAndDFS.java
+++ BreadFirstSearch/Patterns/GraphColoringProblems/isBipartite/SolutionBFSAndDFS.java
@@ -5,12 +5,15 @@ class Solution {
          
         for (int i = 0; i < g.length; i++){
             if (colors[i] == 0) {
-              if(!bfs(g,colors,i)) return false;
+              if(!bfs(g,colors,i)) return false;  
+              //if(!dfs(g,i,colors,1)) return false;
             }
         }
         return true;
     }
     
+    //1 ms, faster than 65.71% 
+    //44.7 MB, less than 63.42%
     private boolean bfs(int[][] g,int[] colors ,int i ){
         Queue<Integer> q = new LinkedList<>();
         q.add(i);
@@ -27,6 +30,23 @@ class Solution {
             }
         }
         
+        return true;
+    }
+    
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions .
+    //Memory Usage: 44.2 MB, less than 63.42% of Java online submissions .
+
+    private boolean dfs(int[][] graph, int current, int[] colors, int color) {
+        if (colors[current] == -color)
+            return false;
+        colors[current] = color;
+        for (int other: graph[current]) {
+            if (colors[other] == color)
+                return false;
+            if (colors[other] == 0)
+                if (!dfs(graph, other, colors, -color))
+                    return false;
+        }
         return true;
     }
    
