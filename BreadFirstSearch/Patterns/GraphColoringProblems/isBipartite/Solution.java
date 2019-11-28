5564a1138b67b983759068c749555a7cb6c72e63
arun <arunsadhasivam@yahoo.co.in>
2019-11-27 16:47:45 -0800

    Create Solution.java

---
+++ BreadFirstSearch/Patterns/GraphColoringProblems/isBipartite/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+     public boolean isBipartite(int[][] g) {
+       int[] colors = new int[g.length];
+         
+         int len = g.length;
+        for (int i = 0; i < len; i++){
+            if (colors[i] == 0) {
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                colors[i] = 1;
+                while (!q.isEmpty()) {
+                    Integer node = q.poll();
+                    for (int adjacent : g[node])
+                        if (colors[adjacent] == colors[node])
+                            return false;
+                        else if (colors[adjacent] == 0) {
+                            q.add(adjacent);
+                            colors[adjacent] = -colors[node];
+                        }
+                }
+            }
+        }
+        return true;
+    }
+   
+}
