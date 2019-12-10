e5d052041fcfaa5a2a6b6b77d6322bb29ac8a97f
arun <arunsadhasivam@yahoo.co.in>
2019-12-09 18:54:56 -0800

    Update simpleSolution.java

--- RecursionBasics/RecursionOrderOfFlow/maxDepthofBinaryTree/simpleSolution.java
+++ RecursionBasics/RecursionOrderOfFlow/maxDepthofBinaryTree/simpleSolution.java
@@ -11,3 +11,18 @@ class Solution {
         
     }
 }
+
+
+//solution 2
+
+
+ public int maxDepth(TreeNode root) {
+        
+        if(root==null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(1+left , 1+right);
+        
+    }
