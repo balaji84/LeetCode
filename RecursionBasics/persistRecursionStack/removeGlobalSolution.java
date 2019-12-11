b90f2584c0016bfb335046e42e12acada27c112f
arun <arunsadhasivam@yahoo.co.in>
2019-12-11 15:23:58 -0800

    Create removeGlobalSolution.java

---
+++ RecursionBasics/persistRecursionStack/removeGlobalSolution.java
@@ -0,0 +1,18 @@
+class Solution {
+  
+    public int findTargetSumWays(int[] nums, int S) {
+        if (nums == null || nums.length == 0) return 0;
+        int count = helper(nums, S, 0, 0,0);
+        return count;
+    }
+    
+    public int helper(int[] nums, int target, int pos, long eval,int count){
+        if (pos == nums.length) {
+            if (target == eval) count++;
+            return count;
+        }
+       count =  helper(nums, target, pos + 1, eval + nums[pos],count);
+       count = helper(nums, target, pos + 1, eval - nums[pos],count);
+        return count;
+    }
+}
