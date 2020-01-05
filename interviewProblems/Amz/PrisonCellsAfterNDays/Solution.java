3e7b5d390eba1340c23952413a4ff0e204d8df71
arun <arunsadhasivam@yahoo.co.in>
2020-01-04 21:24:30 -0700

    Update Solution.java

--- interviewProblems/Amz/PrisonCellsAfterNDays/Solution.java
+++ interviewProblems/Amz/PrisonCellsAfterNDays/Solution.java
@@ -9,10 +9,22 @@ class Solution {
         // Day 5: [0, 1, 1, 1, 0, 1, 0, 0] -4+16+32+64=116
         // Day 6: [0, 0, 1, 0, 1, 1, 0, 0] 
         // Day 7: [0, 0, 1, 1, 0, 0, 0, 0] -16+32=48
+        
+        
+        //[0, 1, 0, 1, 1, 0, 0, 1] 
+        // <-C->
+        
+        //FROM cur if previous and next are same then it is occupied.
+        //previous and next are diff other wise it can be used.
 
         //1+2+4+8+16+32+64+128 =255
                 if ( N == 0 ) return cells;
         N = N%14 == 0 ? 14 : N%14;
+        //cycles repeat every  2 days.
+        //manually need to check after how long it repeats.
+        //important to gets the repeating cycles because for large input for 
+        //1000 days no need to iterate 1000 days we can do 1000%14 =6
+        //so if we check for 6 days that will be enough same will be for 1000 th day.
 
         int[] newCells = new int[8];
 
